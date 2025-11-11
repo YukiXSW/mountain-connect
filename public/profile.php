0385ae5448e0b1bc3b3c3089f2015e7056ca82e6
Lili Wu <lilikw0212@gmail.com>
2025-11-11 19:32:44 +0100

    imagen añadido en profile.php

--- public/profile.php
+++ public/profile.php
@@ -12,7 +12,7 @@ $user = $_SESSION['user'];
     </head>
     <body>
         <div class="profile-card">
-            <img src="../assets/img/avatar_default.png" alt="Avatar" class="avatar">
+            <img src="../assets/images/xiao.jpg" alt="Avatar" class="avatar">
             <h2>Bienvenido, <?= htmlspecialchars($user['username']) ?> 👋</h2>
             <p><strong>Email:</strong> <?= htmlspecialchars($user['email']) ?></p>
             <button class="logout-btn"><a href="logout.php">Cerrar sesión</a></button>
