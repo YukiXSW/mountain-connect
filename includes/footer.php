c10fc28149800d5cfce669478b2ff09abaeab2e1
Lili Wu <lilikw0212@gmail.com>
2025-11-16 17:27:12 +0100

    tabulado

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
-</main>
+    </main>
 
-    <footer class=p-3>
-        <p class="mb-0">&copy; <?php echo date('Y'); ?> MountainConnect. Todos los derechos reservados.</p>
-    </footer>
+        <footer class=p-3>
+            <p class="mb-0">&copy; <?php echo date('Y'); ?> MountainConnect. Todos los derechos reservados.</p>
+        </footer>
 
-</body>
+    </body>
 </html>
