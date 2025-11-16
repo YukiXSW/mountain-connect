5cab1a67bafce1bc8232fda735989e764698cdac
Lili Wu <lilikw0212@gmail.com>
2025-11-16 17:26:53 +0100

    funcion para verificar la sesion en el archivo auth_check.php

--- includes/auth_check.php
+++ includes/auth_check.php
@@ -1,9 +1,16 @@
 <?php 
-# pagina de perfil protegida
-session_start(); # Inicia la sesión PHP
-# validación de sesion
-if (!isset($_SESSION['user'])) { # si no existe la sesión de usuario
-    header('Location: ../login.php'); # redirige a login
-    exit; 
-} # si existe, continúa al perfil
+# activar la sesión
+
+function verificarSesion(){
+    // Iniciar sesión si no está activa la sesion
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    if (!isset($_SESSION['user'])) { # si no existe la sesión de usuario
+        header('Location: ../login.php');# redirige a login
+        exit();
+    } # si existe, continúa al perfil
+}
+
 ?>
